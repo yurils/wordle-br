900d995b5be4fb38318f2d2ec24416825962accb
Yuri Leal <yurileall@gmail.com>
2025-02-01 16:04:17 -0300

    adicionar .nojekyll

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wordle/",
+  base: "/wordle-br/",
   plugins: [svelte({
     preprocess: vitePreprocess()
   })],
